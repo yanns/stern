672df4d9fbcd680146d71d422d244f3a12ae23ac
Yann Simon <simon@adesso.de>
2011-09-23 14:30:15 +0200

    initial commit

---
+++ test/ApplicationTest.java
@@ -0,0 +1,26 @@
+import org.junit.*;
+
+import play.Logger;
+import play.templates.TemplateLoader;
+import play.test.*;
+import play.mvc.*;
+import play.mvc.Http.*;
+import models.*;
+
+public class ApplicationTest extends FunctionalTest {
+
+    @Test
+    public void testThatIndexPageWorks() {
+        for (int i = 0 ; i < 20 ; i++) {
+            for (int j = 0 ; j < 10000 ; j++) {
+                Response response = GET("/" + j);
+                assertIsOk(response);
+                assertContentType("text/html", response);
+                assertCharset(play.Play.defaultWebEncoding, response);
+            }
+            Logger.info("clean template compiled cache");
+            TemplateLoader.cleanCompiledCache();
+        }
+    }
+
+}
